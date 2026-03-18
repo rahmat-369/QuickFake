72777d4e1ef1a7277d53037ae3742dd59da0f686
R_hmt dev <rohmatulloh.3609@gmail.com>
2026-03-18 13:45:21 +0700

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const FormData = require('form-data');
+
+// API Key ImgBB kamu (Ditaruh di sini agar aman di sisi server)
+const IMGBB_KEY = '4f2dcd79dcf47d778991fc93e3320c09'; 
+const BASE_URL = 'https://api.zenzxz.my.id'; 
+
+async function uploadToImgBB(fileBuffer) {
+    const form = new FormData();
+    form.append('image', fileBuffer.toString('base64'));
+    
+    const response = await axios.post(`https://api.imgbb.com/1/upload?key=${IMGBB_KEY}`, form);
+    return response.data.data.url;
+}
+
+async function handleRequest(fields, files) {
+    const { endpoint } = fields;
+    let params = new URLSearchParams();
+
+    // Loop semua field teks
+    for (let key in fields) {
+        if (key !== 'endpoint') params.append(key, fields[key]);
+    }
+
+    // Loop semua file dan upload otomatis
+    for (let file of files) {
+        const uploadedUrl = await uploadToImgBB(file.buffer);
+        // Map field file ke parameter API asli (avatar/url/content)
+        params.append(file.fieldname, uploadedUrl);
+    }
+
+    // Bangun URL akhir untuk menembak API Zenzxz
+    return `${BASE_URL}${endpoint}?${params.toString()}`;
+}
+
+module.exports = { handleRequest };
